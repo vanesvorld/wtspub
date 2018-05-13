8c6b5285c308a1c355c539f929633b02feb6b721
vanesvorld <white@electric.mygbiz.com>
2018-05-13 17:36:49 -0400

    ......hanging quote

--- script2.sh
+++ script2.sh
@@ -2,7 +2,7 @@
 #
 bus=$(lsusb -d 09ae:4004 |grep -o -P 'Bus \K\d{3}') 
 device=$(lsusb -d 09ae:4004 |grep -o -P 'Device \K\d{3}') 
-tlpath="/dev/bus/usb/"$bus"/"$device" 
+tlpath="/dev/bus/usb/"$bus"/"$device"" 
 hostname -A ; echo ".hn"
 ls /etc/openvpn/|grep -Po '\d{1,11}.conf' 
 cat /etc/resolv.conf|grep -Po '\d{5}' ; echo ".resolv"
