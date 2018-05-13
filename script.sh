6f6301075b3a6505120293ae7a7e364ba564949b
vanesvorld <white@electric.mygbiz.com>
2018-05-13 17:48:17 -0400

    Fixes

--- script.sh
+++ script.sh
@@ -2,11 +2,12 @@
 #
 bus=$(lsusb -d 09ae:4004 |grep -o -P 'Bus \K\d{3}') 
 device=$(lsusb -d 09ae:4004 |grep -o -P 'Device \K\d{3}') 
-tlpath="/dev/bus/usb/"$bus"/"$device" 
-hostname -A ; echo ".hn"
+tlpath="/dev/bus/usb/"$bus"/"$device"" 
+echo hostname -A".hn"
 ls /etc/openvpn/|grep -Po '\d{1,11}.conf' 
-cat /etc/resolv.conf|grep -Po '\d{5}' ; echo ".resolv"
-ping -c 1 192.168.2.98 2>&1 >/dev/null ; echo ".switch"
+echo cat /etc/resolv.conf|grep -Po '\d{5}'".resolv"
+echo ping -c 1 192.168.2.98 2>&1 >/dev/null".switch"
 #lsusb -d 09ae:4004 
-usbperm=$(sudo ls -la "${tlpath}"|grep -c 'crw-rw-rw-') ; echo ".upsperm"
+usbperm=$(ls -la "${tlpath}"|grep -c 'crw-rw-rw-')
+echo $usbperm
 echo "Ok!"
